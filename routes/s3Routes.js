2c2d1d65f16901d9175a94269c8153bf910c08b0
Liem Nguyen <liemnguyen0111@gmail.com>
2021-03-26 17:51:52 -0700

    update README.md

--- routes/s3Routes.js
+++ routes/s3Routes.js
@@ -3,18 +3,21 @@ const router = require("express").Router();
 
 // Import middlewares
 const utils = require("../upload/index.js");
-const { upload, s3 } = utils;
+const { upload, uploadS3, viewS3 } = utils;
 
 // Get route - (Read)
-router.get("/files", (req, res) => {
-  res.json(200);
+router.get("/files", async (req, res) => {
+    // console.log(viewS3())
+  const data = await viewS3().then((e) => e)
+ 
+  res.status(200).send(data);
 });
 
 // Post route - (Create)
 router.post("/files", upload, async (req, res) => {
 
   // Upload file(s) into s3 bucket and return a location of file(s). If no files exist, return empty array
-  const data = await s3(req.files).then((e) => e);
+  const data = await uploadS3(req.files).then((e) => e);
 
   res.status(200).send(data);
 });
--- upload/index.js
+++ upload/index.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const AWS = require("aws-sdk");
 
 // Variables
-// Access to AWS S3 
+// Access to AWS S3
 const s3 = new AWS.S3({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -18,12 +18,12 @@ const storage = multer.memoryStorage({
 });
 
 //--- S3 Upload function ---//
-const s3Upload = (file) => {
+const s3Upload = async (file) => {
   // Split the file name. Expample : "my-file.png" => ["my-file", "png"]
   const myFile = file.originalname.split(".");
   // File attributes
   const data = {
-    originalName : file,
+    originalName: file,
     fileName: myFile[0],
     fileType: myFile[myFile.length - 1],
     contentType: file.mimetype,
@@ -36,22 +36,38 @@ const s3Upload = (file) => {
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME,
     Key: `${data.fileName}-${Date.now()}.${data.fileType}`,
-    key : data.originalName,
+    key: data.originalName,
     ContentEncoding: data.contentEncoding,
     ContentDisposition: data.contentDisposition,
     ContentType: data.contentType,
     Body: data.buffer,
   };
+
   return s3.upload(params).promise();
+};
+
+const s3View = async () => {
+  try {
+    return await s3
+      .listObjectsV2({
+        Bucket: process.env.AWS_BUCKET_NAME,
+      })
+      .promise();
+  } catch (err) {
+    return err;
+  }
 };
 
 // Upload utils
 const utils = {
-    // How file(s) are stored with multer
-    upload: multer({ storage }).any("file"),
-    // Upload file(s) onto s3 bucket and return a promises
-    s3: (files) => Promise.all(files ? files.map((file) => s3Upload(file)) : []),
-  };
+  // How file(s) are stored with multer
+  upload: multer({ storage }).any("file"),
+  // Upload file(s) onto s3 bucket and return a promises
+  uploadS3: (files) =>
+    Promise.all(files ? files.map((file) => s3Upload(file)) : []),
+  // View bucket
+  viewS3: s3View,
+};
 
 // Export utils
 module.exports = utils;
